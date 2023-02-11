01442158fa772f046808888ce164d84d5f2ffe3d
vishal kumar <117971824+vishal9934@users.noreply.github.com>
2023-02-11 22:26:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,47 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+     arr.map(arr => {
+        if (arr.marks > 50) {
+          console.log(arr);
+        }
+      }); 
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+    arr.forEach(data => {
+        if (data.marks > 50) {
+            console.log(data);
+          }
+    })
 }
 
 function addData() {
   //Write your code here, just console.log
+     let obj = {id:4,name:"susan",age:"20",marks:45};
+  arr.push(obj);
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  for(let i = 0; i <= arr.length; i++){
+        if(arr[i].marks <= 50){
+          arr.splice(i, i + 1);
+        }
+      }
+      console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+   let ids = arr.length;
+    let arr1 = [
+      { id: ids + 1, name: "vishal", age: "22", marks: 10 },
+      { id: ids + 2, name: "neha", age: "28", marks: 11 },
+      { id: ids + 3, name: "noobdi", age: "24", marks: 100 },
+    ];
+    let array = arr.concat(arr1);
+    console.log(array)
 }
